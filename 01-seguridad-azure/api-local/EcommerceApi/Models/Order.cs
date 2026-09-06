6570319e18adeb3efcc210918ab93f8d3c658d5f
Lorenzo Lanuza <lanuzalorenzo@gmail.com>
2026-09-06 18:48:01 +0200

    Cleanup: remove API Local from portfolio after migration to independent lab

--- 01-seguridad-azure/api-local/EcommerceApi/Models/Order.cs
+++
@@ -1,9 +0,0 @@
-namespace EcommerceApi.Models;
-
-public class Order
-{
-    public int Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public List<int> ProductIds { get; set; } = new List<int>();
-    public decimal Total { get; set; }
-}
